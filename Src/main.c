e0253255ecf73534a2750221a3725edc057b1dca
dltjdrn1123 <dltjdrn1123@gmail.com>
2021-04-14 17:58:30 +0900

    ''

--- Src/main.c
+++ Src/main.c
@@ -123,7 +123,18 @@ void board_detect();
 extern void initialise_monitor_handles(void);
 #endif
 
-/* USER CODE END 0 */
+
+
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+}
 
 int main(void)
 {
@@ -170,8 +181,10 @@ int main(void)
   // reinitialize uart with speed from config
   huart2.Init.BaudRate = USART_DEBUG_SPEED;
   HAL_UART_Init(&huart2);
-
+  
   DEBUG_PRINTF("Hello, Lepton!\n\r");
+
+
   fflush(stdout);
 
   lepton_init();
